45cc0192a3cddd133727f2960e66b16f3926c71c
Andres <andresssss10@gmail.com>
2018-08-30 10:07:15 -0500

    Lodging views

---
+++ app/src/main/java/javerana/edu/co/proyectocompumovil/LodgingHistory.java
@@ -0,0 +1,13 @@
+package com.example.tauret.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LodgingHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lodging_history);
+    }
+}
---
+++ app/src/main/java/javerana/edu/co/proyectocompumovil/Reservation_info.java
@@ -0,0 +1,13 @@
+package com.example.tauret.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Reservation_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_info);
+    }
+}
