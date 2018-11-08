0331923791d7922abf22a6adbf6540c9994149ea
Andres <niseandres@gmail.com>
2018-11-08 06:59:55 -0500

    Merge completo, alojamientos, mapa, calendario

--- app/src/main/java/com/example/andre/jbookingmobile/MisAlojamientosFragment.java
+++ app/src/main/java/com/example/andre/jbookingmobile/MisAlojamientosFragment.java
@@ -1,6 +1,7 @@
 package com.example.andre.jbookingmobile;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.example.andre.jbookingmobile.CrearAlojamientos.CrearAlojamiento1;
 
 public class MisAlojamientosFragment extends Fragment {
 
@@ -31,7 +34,7 @@ public class MisAlojamientosFragment extends Fragment {
         buttonAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(getContext(),CrearAlojamiento1.class));
             }
         });
     }
