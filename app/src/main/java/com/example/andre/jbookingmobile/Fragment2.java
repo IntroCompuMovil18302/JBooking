3b374c915df6b15dcf4a785e76431ab47124790b
alejandrotorres1350 <alejo.novoa@misena.edu.co>
2018-11-14 15:44:59 -0500

    Historial de reservas

--- app/src/main/java/com/example/andre/jbookingmobile/Fragment2.java
+++ app/src/main/java/com/example/andre/jbookingmobile/Fragment2.java
@@ -2,22 +2,47 @@ package com.example.andre.jbookingmobile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.Toast;
 
+import com.example.andre.jbookingmobile.Adapters.AlojamientoAdaptador;
+import com.example.andre.jbookingmobile.Adapters.ReservaAdaptador;
 import com.example.andre.jbookingmobile.CrearAlojamientos.CrearAlojamiento1;
 import com.example.andre.jbookingmobile.CrearAlojamientos.CrearAlojamiento3;
+import com.example.andre.jbookingmobile.Entities.Alojamiento;
+import com.example.andre.jbookingmobile.Entities.Reserva;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Fragment2 extends Fragment {
 
     private  Button newAlj ;
+
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private List<Reserva> reservas;
+    private GridView gridViewReservas;
+    private ReservaAdaptador reservaAdaptador;
+
+    public static final String PATH_RESERVAS = "reservas/";
+
 
     public Fragment2() {
         // Required empty public constructor
@@ -27,19 +52,63 @@ public class Fragment2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_fragment2,
-                container, false);
 
-        newAlj = view.findViewById(R.id.butNewAloj);
-        newAlj.setOnClickListener(new View.OnClickListener() {
+        View view = inflater.inflate(R.layout.fragment_fragment2, container, false);
+        database = FirebaseDatabase.getInstance();
+        reservas = new ArrayList<>();
+        reservaAdaptador = new ReservaAdaptador(getContext(), reservas);
+        gridViewReservas = view.findViewById(R.id.gridViewFragment1Reservas);
+
+        gridViewReservas.setAdapter(reservaAdaptador);
+
+        initEvents();
+        cargarReservas();
+        return view;
+    }
+
+    private void cargarReservas(){
+        myRef = database.getReference(PATH_RESERVAS);
+        myRef.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(),"No tiene sentido la seleccion actual ",Toast.LENGTH_SHORT).show();
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Reserva reserva = dataSnapshot.getValue(Reserva.class);
+                reservas.add(reserva);
+                reservaAdaptador.notifyDataSetChanged();
+            }
 
-                Intent intent = new Intent(getActivity(),CrearAlojamiento1.class);
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void initEvents(){
+        gridViewReservas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Reserva ReservaActual = (Reserva) parent.getItemAtPosition(position);
+                Intent intent = new Intent(getContext(),ReservaDetalleActivity.class);
+                Log.i("ENE",ReservaActual.getId()+"HOLAAA");
+                intent.putExtra("reserva",(Serializable) ReservaActual);
                 startActivity(intent);
             }
         });
-        return inflater.inflate(R.layout.fragment_fragment2, container, false);
+
     }
 }
---
+++ app/src/main/java/com/example/andre/jbookingmobile/ReservaDetalleActivity.java
@@ -0,0 +1,121 @@
+package com.example.andre.jbookingmobile;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.andre.jbookingmobile.Entities.Alojamiento;
+import com.example.andre.jbookingmobile.Entities.Reserva;
+import com.squareup.picasso.Picasso;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class ReservaDetalleActivity extends AppCompatActivity {
+
+    private Reserva reserva;
+    private Toolbar toolbar;
+
+    private LinearLayout linearLayoutGaleria;
+    private LayoutInflater inflater;
+
+    private TextView textViewLugar;
+    private TextView textViewNombre;
+    private TextView textViewPrecio;
+
+    private TextView textViewTipo;
+    private TextView textViewServicios;
+
+    private TextView textViewDesde;
+    private TextView textViewHasta;
+
+    private Button buttonComentarios;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserva_detalle);
+
+        toolbar =  findViewById(R.id.appbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        textViewLugar = findViewById(R.id.textViewReservaDetalleLugar);
+        textViewNombre = findViewById(R.id.textViewReservaDetalleNombre);
+
+        textViewPrecio = findViewById(R.id.textViewReservaDetallePrecio);
+        textViewTipo = findViewById(R.id.textViewReservaDetalleTipo);
+
+        textViewServicios = findViewById(R.id.textViewReservaDetalleServicios);
+        textViewDesde = findViewById(R.id.textViewFrom);
+        textViewHasta = findViewById(R.id.textViewTo);
+
+        buttonComentarios = findViewById(R.id.buttonReservaDetalleComentario);
+
+        reserva = (Reserva) getIntent().getExtras().getSerializable("reserva");
+
+        linearLayoutGaleria = findViewById(R.id.linearLayoutGaleria);
+        inflater = LayoutInflater.from(this);
+        initEvents();
+        cargarVista();
+    }
+
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.trans_right_in,
+                R.anim.trans_right_out);
+    }
+
+    private void cargarVista(){
+        List<String> imagenes = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(reserva.getAlojamiento().getFotos(),";");
+        while (st.hasMoreTokens()){
+            imagenes.add(st.nextToken());
+        }
+
+        for (String img: imagenes){
+            View view = inflater.inflate(R.layout.imagen_alojamiento_item, linearLayoutGaleria,false);
+            ImageView imagenActual = view.findViewById(R.id.imageViewAlojamientoDetalleFoto);
+            Picasso.with(ReservaDetalleActivity.this).load(img).into(imagenActual);
+            linearLayoutGaleria.addView(view);
+        }
+
+        textViewLugar.setText(reserva.getAlojamiento().getUbicacion().getNombre());
+        textViewNombre.setText(reserva.getAlojamiento().getNombre());
+        textViewTipo.setText(reserva.getAlojamiento().getTipoPropiedad());
+        textViewServicios.setText(reserva.getAlojamiento().getServicios()+"");
+        textViewPrecio.setText("$"+reserva.getValor());
+        String fecha1 = reserva.getFechaInicio().getDay()+"/"+reserva.getFechaInicio().getMonth()+"/"+reserva.getFechaInicio().getYear();
+        String fecha2 = reserva.getFechaFin().getDay()+"/"+reserva.getFechaFin().getMonth()+"/"+reserva.getFechaFin().getYear();
+        textViewDesde.setText(fecha1);
+        textViewHasta.setText(fecha2);
+    }
+
+    private void initEvents(){
+        buttonComentarios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent intent = new Intent(ReservaDetalleActivity.this,ComentariosActivity.class);
+                intent.putExtra("reserva",(Serializable) reserva);
+                startActivity(intent);*/
+            }
+        });
+    }
+}
